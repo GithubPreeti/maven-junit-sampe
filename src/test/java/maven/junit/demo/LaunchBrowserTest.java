c9e7515ff81800474f8cf6234cbfc0b56cc01802
GithubPreeti <40623376+GithubPreeti@users.noreply.github.com>
2018-07-17 22:14:17 +0530

    editing path of driver

--- src/test/java/maven/junit/demo/LaunchBrowserTest.java
+++ src/test/java/maven/junit/demo/LaunchBrowserTest.java
@@ -11,7 +11,7 @@ public class LaunchBrowserTest {
 	public WebDriver driver;
 	@Test
 	public void launchBrowser() {
-		System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
 		driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
         assertEquals("Google", driver.getTitle());
