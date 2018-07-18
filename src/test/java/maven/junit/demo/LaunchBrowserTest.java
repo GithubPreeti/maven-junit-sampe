428f35ad135a313d5309f91a9a73c4d1de6fadb9
GithubPreeti <40623376+GithubPreeti@users.noreply.github.com>
2018-07-18 17:52:53 +0530

    Update LaunchBrowserTest.java

--- src/test/java/maven/junit/demo/LaunchBrowserTest.java
+++ src/test/java/maven/junit/demo/LaunchBrowserTest.java
@@ -11,8 +11,10 @@ public class LaunchBrowserTest {
 	public WebDriver driver;
 	@Test
 	public void launchBrowser() {
+		ChromeOptions ChromeOptions = new ChromeOptions();
+ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
 		System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(ChromeOptions);
 		driver.get("https://www.google.com/");
         assertEquals("Google", driver.getTitle());
 		driver.close();
